9525fce96ad6d0c04411a9bd0d8bcf87466aeac3
web-coder-pro <oralboyf@gmail.com>
2025-03-30 13:22:03 +0500

    Create 3-program.kt

---
+++ 3-program.kt
@@ -0,0 +1,15 @@
+// Oddiy  if operatori
+fun main() {
+    val yosh = 18
+    
+    if (yosh >= 18) {
+        println("Siz voyaga yetgansiz.")
+    
+    val age = 16
+    
+    if (age >= 18) {
+        println("Siz kattasiz")
+    } else {
+        println("Siz kichiksiz")
+    }
+}
