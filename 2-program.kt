b2bdfb650df331b231fd20f92090b72d60d12ea0
web-coder-pro <oralboyf@gmail.com>
2025-03-30 13:21:26 +0500

    Create 2-program.kt

---
+++ 2-program.kt
@@ -0,0 +1,31 @@
+fun main() {
+    // Arifmetik operator
+    val a = 10
+    val b = 2
+    
+    println(a + b)
+    println(a - b)
+    println(a / b)
+    println(a * b)
+    
+    // Oʻzgaruvchilarni oshirish va kamaytirish
+    val son = 5
+    println(son + 1)
+    println(son)
+    println(son - 1)
+    println(son)
+    
+    // Taqqoslash operatori
+    val n = 10
+    val k = 5
+    
+    println(a < b)
+    println(a > b)
+    println(a == b)
+    
+    // Mantiqiy operator
+    val c = 5
+    val d = 10
+    
+    println(c > 0 && d > 0)
+}
