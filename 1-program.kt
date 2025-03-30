f60ff5b071469535159ea4cede9a764eda80dd38
web-coder-pro <oralboyf@gmail.com>
2025-03-30 13:20:14 +0500

    Create 1-program.kt

---
+++ 1-program.kt
@@ -0,0 +1,21 @@
+// Oʻzgaruvchilar
+fun main() {
+    var yosh = 14
+    val ism = "Abdulvahhob"
+    
+    println(ism)
+    println(yosh)
+    
+// Malumot turlari
+    var son: Double = 4.5
+    var baho: Int = 5
+    var javob: Boolean = true
+    
+    println(son)
+    println(baho)
+    println(javob)
+ 
+    val name = "Abdulvahhob"
+    
+    println("Mening ismim $name")
+}
